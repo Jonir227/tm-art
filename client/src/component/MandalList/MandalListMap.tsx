8e8a82872579886584fd94cf19a6b396b487a9e4
Kim Bum June <bjbj6363@gmail.com>
2018-12-27 11:42:05 +0900

    리스트 아이템 스타일 수정

--- client/src/component/MandalList/MandalListMap.tsx
+++ client/src/component/MandalList/MandalListMap.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { GreenColor } from '../../constants/colors';
 import { IMandalArtFront } from '../../types/MandalArt';
 import { getGrade } from '../../utils';
 
-const TileWrapper = styled(Link)`
-  background-color: yellowgreen;
+const TileWrapper = styled.li`
+  margin: 1px;
+`;
+
+const TileLink = styled(Link)`
+  width: 146px;
+  height: 190px;
+  background-color: ${GreenColor.light};
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  &::before {
+    content: '';
+    width: 100%;
+    height: 60px;
+    background-color: white;
+  }
+`;
+
+const TileIcon = styled.div`
+  border-radius: 50%;
+  background-color: white;
+  width: 80px;
+  height: 80px;
+  margin-top: -35px;
+  padding-right: 7px;
+  line-height: 90px;
+  font-size: 60px;
+  text-align: center;
+`;
+
+const TileGoal = styled.div`
+  padding: 20px 5px 0px 5px;
+  max-width: 90%;
+  word-wrap: break-word;
+  text-align: center;
 `;
 
 interface IMandalListTile {
@@ -16,23 +52,32 @@ interface IMandalListTile {
 
 const MandalListTile = ({ id, goal, score }: IMandalListTile) => {
   return (
-    <TileWrapper to={`/${id}`}>
-      <div>{goal}</div>
-      <div>{getGrade(score)}</div>
+    <TileWrapper>
+      <TileLink to={`/${id}`}>
+        <TileIcon>{getGrade(score)}</TileIcon>
+        <TileGoal>{goal}asdlkfjasldkfjalkdsfjaslkj</TileGoal>
+      </TileLink>
     </TileWrapper>
   );
 };
+
+const ListMapWrapper = styled.ul`
+  padding: 0px 11px 0px 11px;
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+`;
 
 interface IMandalListMap {
   mandalArtList: IMandalArtFront[];
 }
 
 const MandalListMap = ({ mandalArtList }: IMandalListMap) => (
-  <>
+  <ListMapWrapper>
     {mandalArtList.map(mandalArtFront => (
       <MandalListTile key={mandalArtFront.id} {...mandalArtFront} />
     ))}
-  </>
+  </ListMapWrapper>
 );
 
 export default MandalListMap;
